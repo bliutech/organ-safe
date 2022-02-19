719e55dabe82f20644f1e5b98de443e42803e96b
bliutech <bensonhliu@gmail.com>
2022-02-19 02:11:23 -0800

    initialized frontend. added basic file structure fo components and pages. implemented multipages using BrowserRouter.

---
+++ frontend/src/pages/Matches.js
@@ -0,0 +1,8 @@
+export default function Matches(){
+    return(
+        <div>
+            <h1> Matches </h1>
+            <p> A secure organ matching website supported by blockchain technology! </p>
+        </div>
+    );
+}
